18cff112602390a3e7bf797ea31469243b6443e4
Andrew Carr <11998657+editedbaseline@users.noreply.github.com>
2021-01-14 13:54:15 +0000

    Migrated to pycryptodome

--- generateRSAkey.py
+++ generateRSAkey.py
@@ -1,27 +1,32 @@
-# Generate RSA keys
+'''
+Generate RSA keys
+CT5046
+
+https://www.pycryptodome.org/en/latest/src/examples.html#generate-an-rsa-key
+https://stackoverflow.com/questions/56923762/crypto-cipher-aes-mode-eax-encrypt-and-digest-error-argument-2-must-be-bytes-n
+
+The Crypto module must be pycryptodome (not pycrypto, and it won't run it both are installed)
+'''
 
 from Crypto.PublicKey import RSA
-from Crypto import Random
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
 
-# Get random number
-random_generator = Random.new().read
-
-# Generate key pair
-key = RSA.generate(1024, random_generator)
+# Generate keys
+key = RSA.generate(2048)
+session_key = get_random_bytes(16)
 print(key)
 
-# Represent plaintext as byte string
+# Plaintext as UTF-8
 strPlaintext = b'RSA encryption is easy'
 
-# Get public key
-key_public = key.publickey()
+# Encrypt session key with RSA public key
+cipher_rsa = PKCS1_OAEP.new(key)
+enc_session_key = cipher_rsa.encrypt(session_key)
 
-# Encrypt the plaintext using it
-strCiphertext = key_public.encrypt(strPlaintext, 32)
-print(strCiphertext)
+# Encrypt data with session key
+cipher_aes = AES.new(session_key, AES.MODE_EAX)
+ciphertext, tag = cipher_aes.encrypt_and_digest(strPlaintext)
 
-# Decrypt ciphertext
-strDecryptedText = key.decrypt(strCiphertext)
-print(strCiphertext)
-
-#Signed?
+# Print ciphertext
+print("Ciphertext is", ciphertext)
